e5d4855c5b5e48aa575b26cdfe5dcdb3d865596f
Kenneth Felix <kennf03@gmail.com>
2021-09-14 12:34:56 -0400

    Adding modules to github

---
+++ PREWORK_KNF/Module-6/javascript.js
@@ -0,0 +1,17 @@
+function toGrow() {
+    document.getElementById("box").style.height = "250px";
+    document.getElementById("box").style.width = "250px";
+}
+function beBlue() {
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+function toFade() {
+    document.getElementById("box").style.opacity = ".5";
+}
+function toReset() {
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1";
+
+}
